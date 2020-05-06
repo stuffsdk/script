50124989ca6e3fa5d3a4f139ae7d5820cf65aee7
stuffsdk <64832457+stuffsdk@users.noreply.github.com>
2020-05-06 11:48:31 +0530

    Update stuffsdk.sh

--- stuffsdk.sh
+++ stuffsdk.sh
@@ -1,4 +1,25 @@
 #!/bin/bash
 
-echo 'NEW UPDATE';
-echo 'Thanks';
+
+case "$1" in
+   "make")
+     if [ -e $HOME/stuffsdk-src ]
+        then
+             git pull
+             echo 'done!';
+        else
+             git clone git@github.com:stuffsdk/stuffsdk.git $HOME/stuffsdk-src
+             echo 'done!';
+     fi
+   ;;
+   "runserver")
+     php -S localhost:1508
+   ;;
+   "startproject")
+     git clone https://github.com/stuffsdk/project.git $2
+     echo "project created successfully.";
+   ;;
+   *)
+     echo "Stuffsdk Version: 1.0.0";
+   ;;
+esac
